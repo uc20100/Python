884238136710561c09fa89679abcc17502c1fcdd
UC <33086329+uc20100@users.noreply.github.com>
2022-09-25 16:55:25 +0400

    Исправил небольшой баг

--- task007.py
+++ task007.py
@@ -10,7 +10,7 @@ def test_bool(x, y, z):
     value1 = ~(x|y|z) & (2**max_len-1)
     print(f'¬(x ⋁ y ⋁ z) -> ¬({x:0{max_len}b} ⋁ {y:0{max_len}b} ⋁ {z:0{max_len}b}) = {value1:0{max_len}b}')
     value2 = ~x & ~y & ~z & mask
-    print(f'¬(x) ⋀ ¬(y) ⋀ ¬(z) -> {(~x&mask):0{max_len}b} ⋀ {(~y&mask):0{max_len}b} ⋀ {(~z&mask):0{max_len}b} = {value1:0{max_len}b}')
+    print(f'¬(x) ⋀ ¬(y) ⋀ ¬(z) -> {(~x&mask):0{max_len}b} ⋀ {(~y&mask):0{max_len}b} ⋀ {(~z&mask):0{max_len}b} = {value2:0{max_len}b}')
     return value1==value2
 
 
